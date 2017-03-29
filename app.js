6d24d91d774d3288e434c2c326bdbe48f4a80d48
Karina Gaulin <karinagaulin@gmail.com>
2017-03-29 15:33:35 -0400

    Redo

--- app.js
+++ app.js
@@ -1,29 +1,88 @@
 
+$("document").ready(function() {
+console.log("Here");
 var YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
-
+  
 function getDataFromApi(searchTerm, callback) {
   var query = {
-    
     part: "snippet",
     key: "AIzaSyAuQdJmLgDckMmHbOLCqkxAz81L7OsonN0",
-    q: "search"
+    q: searchTerm
   };
-  $.getJSON(YOUTUBE_BASE_URL, query, callback);
+console.log(".line 10");
+
+$.getJSON(YOUTUBE_BASE_URL, query, function(x, y, z) {
+  console.log("first", x, "second",y, "third", z);
+  console.log(x.items);
+  for(var i = 0; i < x.items.length; i++) {
+    console.log(x.items[i]);
+    const item = x.items[i]; 
+
+    $(".js-search-results").append("<li>" + item.snippet.channelTitle + "<img src='"+ item.snippet.thumbnails.default.url + "'</li>");
+  }
+
+});
+
+}
+
+$("button").click(function(event) {
+  event.preventDefault();
+  console.log("clicked");
+  getDataFromApi($("#search-box").val());
+
+})
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function getDataFromApi(searchTerm, callback) {
+  var query = {
+    part: "snippet",
+    key: "AIzaSyAuQdJmLgDckMmHbOLCqkxAz81L7OsonN0",
+    q: searchTerm,
+   
+  };
+  $.getJSON(YOUTUBE_BASE_URL, query, showYouTubeData);
+  console.log(query.q);
+  console.log(query.q[0]);
 }
 
 
 function showYouTubeData(data) {
+  console.log(data);
   var resultElement = '';
-  if (data.Search) {
-    data.Search.forEach(function(item) {
-     resultElement += '<p>' + item.Title + '</p>';
-     console.log(item.Title);
+  if (data.items) {
+    console.log(data);
+    console.info("Data is here");
+    console.info(typeof data.items);
+    console.log(data.items[0].snippet.thumbnails.default.url);
+    data.items.forEach(function(item) {
+     resultElement += '<p>' + item.snippet.title
+      + '</p>';
+      resultElement += '<a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img class="thumbnail" src="' + item.snippet.thumbnails.default.url
+      + '"/></a>';
+     console.log(item.snippet.title);
      
     });
   }
   else {
     resultElement += '<p>No results</p>';
   }
+  
   
   $('.js-search-results').html(resultElement);
 }
@@ -35,32 +94,5 @@ function watchSubmit() {
     getDataFromApi(query, showYouTubeData);
   })
 }
-$(function()[watchSubmit();});
-
-
-
-/*$(function() {
-  var OMDB_BASE_URL = 'https://www.omdbapi.com/?';
-
-
-  // this is the query structure -->  https://www.omdbapi.com/?s=mad%20max&r=json
-
-  var paramaters = {
-    s: 'Max Max',
-    r: 'json'
-  };
-
-  function ShowData(results) {
-    console.log('results ', results);
-    console.log(results.Search);
-    $.each(results.Search, function (i, v) {
-      console.log('v is   ', v);
-      $('ul').append('<li>' + v.Title + '</li>');
-    });
-  }
-
-  // url to api, how to use the api, how to display the contents from an api. 
-  $.getJSON(OMDB_BASE_URL, paramaters, ShowData);
-
-});
+$(function(){watchSubmit();});
 */
